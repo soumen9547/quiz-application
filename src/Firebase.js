31ce98549955fcef66ac2719d92bbe4de55ea9ce
soumen9775 <soumen@teminos.com>
2022-08-27 21:07:22 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const App = initializeApp({
-  apiKey: "AIzaSyCuiVSB1tLhB5ecNLzWQp4BHMUtqvZ_v2k",
-  authDomain: "react-quiz-dev-4e747.firebaseapp.com",
-  databaseURL: "https://react-quiz-dev-4e747-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "react-quiz-dev-4e747",
-  storageBucket: "react-quiz-dev-4e747.appspot.com",
-  messagingSenderId: "716651891568",
-  appId: "1:716651891568:web:674c6760d4cb67acf29db5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL:process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT,
+  storageBucket:process.env.REACT_APP_BUCKET,
+  messagingSenderId:process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 });
 export default App;
